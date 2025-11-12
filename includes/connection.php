0c18a2acdc23a87107f2733b53c88fac4e3d3d89
Suman-Thapa <sumanthapa302702@gmail.com>
2025-11-12 07:59:37 +0545

    testing git igore

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost","samriddha","samriddha123","HotelBooking");
+$con=mysqli_connect("localhost","suman","suman123","HotelBooking");
 if(!$con){
     die("Can not Connect");
 }
