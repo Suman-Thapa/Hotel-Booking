096e3b9d7df8ae30fd438efd27727ed996959b73
Suman-Thapa <sumanthapa302702@gmail.com>
2025-11-13 15:05:49 +0545

    Deleting setting.php file

--- payment/setting.php
+++
@@ -1,5 +0,0 @@
-<?php
-$epay_url = "https://rc-epay.esewa.com.np/api/epay/main/v2/form";
-$merchant_code = "EPAYTEST"; // eSewa test merchant code
-$secret_key = "8gBm/:&EnhH.1/q"; // eSewa test secret key
-?>
